1476aac0704f4df92d183da4f4d0ed03b0e6f3c2
alexandremj-360solved <alexandremj@360solved>
2024-12-03 11:50:33 +0000

    set while loop to render menu after running a script

--- dynamoDbManager.sh
+++ dynamoDbManager.sh
@@ -10,35 +10,39 @@ echo "**************************************************************************
 
 # Main menu
 main_menu() {
-    echo "Select an option:"
-    echo "1 - Clean up DynamoDB tables"
-    echo "2 - Export DynamoDB table data"
-    echo "3 - Import DynamoDB table data"
-    echo "4 - Exit"
-    read -p "Enter your choice: " OPTION
+    while true; do
+        echo "Select an option:"
+        echo "1 - Clean up DynamoDB tables"
+        echo "2 - Export DynamoDB table data"
+        echo "3 - Import DynamoDB table data"
+        echo "4 - Exit"
+        read -p "Enter your choice: " OPTION
 
-    case $OPTION in
-        1)
-            echo "Running database cleanup..."
-            ./databaseCleanup.sh
-            ;;
-        2)
-            echo "Running database export..."
-            ./exportData.sh
-            ;;
-        3)
-            echo "Running database import..."
-            ./importData.sh
-            ;;
-        4)
-            echo "Exiting script. Goodbye!"
-            exit 0
-            ;;
-        *)
-            echo "Invalid option. Please select a valid option."
-            main_menu
-            ;;
-    esac
+        case $OPTION in
+            1)
+                echo "Running database cleanup..."
+                ./databaseCleanup.sh
+                echo "Database cleanup completed. Returning to main menu..."
+                ;;
+            2)
+                echo "Running database export..."
+                ./exportData.sh
+                echo "Database export completed. Returning to main menu..."
+                ;;
+            3)
+                echo "Running database import..."
+                ./importData.sh
+                echo "Database import completed. Returning to main menu..."
+                ;;
+            4)
+                echo "Exiting script. Goodbye!"
+                exit 0
+                ;;
+            *)
+                echo "Invalid option. Please select a valid option."
+                ;;
+        esac
+    done
 }
 
 # Start the script by displaying the menu
